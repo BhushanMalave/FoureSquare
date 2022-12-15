402d0dc5050d505de376dcce242a1bdd29530b7f
BhushanMalave <bhushan.malave@robosoftin.com>
2022-12-15 15:22:29 +0530

    added fonts and vector icons

--- App.js
+++ App.js
@@ -9,47 +9,14 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { Login } from './src/screens/LoginScreen';
+import {Login} from './src/screens/LoginScreen';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <Login/>
-    </SafeAreaView>
+    <View style={{flex: 1}}>
+      <Login />
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  ImageBackground,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -7,16 +8,48 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
+  Platform
 } from 'react-native';
 
 export const Login = () => {
-    return(
-        <SafeAreaView>
-            <View>
-                <Text>
-                    login
-                </Text>
+  return (
+    <View style={{flex: 1}}>
+      <ImageBackground
+        source={require('../assets/images/background.png')}
+        style={{flex: 1}}>
+            <SafeAreaView>
+            <View style={styles.viewTop}>
+             <Text style={styles.textskip}>Skip {">"}</Text>
             </View>
-        </SafeAreaView>
-    );
-}
+            <View style={{alignItems:'center',marginTop:30,}}>
+            <Image source={require('../assets/images/logo.png')} style={styles.logo}/>
+            </View>
+            </SafeAreaView>
+          
+        </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    viewTop:{
+        flexDirection:'row',
+        marginTop:Platform.OS === 'android' ? 30 :10,
+        justifyContent:'flex-end',
+        marginRight:20,
+    },
+    textskip:{
+        fontFamily:'Avenir Book',
+        fontSize:28,
+        color:'#FFFFFF',
+    },
+    iconback:{
+        marginTop: Platform.OS === 'android' ? 15 : 10,
+        marginLeft:10,
+    },
+    logo:{
+        
+    }
+
+})
